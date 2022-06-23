1e89196af819751eb0146c964983fd4a7755ac1c
miltinsalgado <miltonsalgadoleandro@gmail.com>
2022-06-23 02:35:04 -0300

    Update Commit

---
+++ DPW - Parte 13 - JS - Introdução/exercicio_06/js/functions.js
@@ -0,0 +1,25 @@
+
+window.onload = function gerarResposta(){
+    const num_h2 = document.getElementsByTagName("h2").length;
+    const num_p = document.getElementsByTagName("p").length;
+    const num_p_info = document.getElementsByClassName("info").length;
+    const num_p_SI = document.getElementsByClassName("SI").length;
+
+    var resposta_1 = "Há " + num_p + " parágrafo(s) e " + num_h2 + " cabeçalho(s) H2 na página (não contando com os adicionados por JavaScript).";
+    const paragrafo_resposta_1 = document.createElement("p");
+    paragrafo_resposta_1.innerHTML = resposta_1;
+    document.body.appendChild(paragrafo_resposta_1);
+
+    var resposta_2 = "Há " + num_p_info + " parágrafo(s) da classe \"info\" e " + num_p_SI + " parágrafo(s) da classe \"SI\"";
+    const paragrafo_resposta_2 = document.createElement("p");
+    paragrafo_resposta_2.innerHTML = resposta_2;
+    document.body.appendChild(paragrafo_resposta_2);
+
+    const segundo_h2 = document.createElement("h2");
+    segundo_h2.innerHTML = document.querySelector("h2:nth-of-type(2)").innerHTML;
+    document.body.appendChild(segundo_h2);
+
+    const terceiro_p = document.createElement("p");
+    terceiro_p.innerHTML = document.querySelector("p:nth-of-type(3)").innerHTML;
+    document.body.appendChild(terceiro_p);
+}
